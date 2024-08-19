4ab37a8ffc2e84faa13a6774f65e0c640170e202
Pedro Villa <pvillarrealmartinez@deloitte.com>
2024-08-19 16:19:48 -0600

    Adding solution for Text exercise

--- Text/Text/Program.cs
+++ Text/Text/Program.cs
@@ -10,7 +10,38 @@ namespace Text
     {
         static void Main(string[] args)
         {
-            
+            StringBuilder sb = new StringBuilder();
+            string paragraph;
+
+            do
+            {
+                Console.WriteLine("Please enter your text (press Enter twice to finish):");
+                paragraph = Console.ReadLine();
+
+                if(!string.IsNullOrWhiteSpace(paragraph))
+                {
+                    sb.AppendLine(paragraph);
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);
+
+            string text = sb.ToString();
+            int countOfWords = -1;
+
+            foreach (string line in text.Split('\n'))
+            {
+                countOfWords += line.Split(' ').Length;
+            }
+
+            int countOfSentences = text.Split(new char[]{'.', '?', '!'}).Length - 1;
+            int countOfParagraphs = text.Split('\n').Length - 1;
+
+            Console.WriteLine($"# of words: {countOfWords}");
+            Console.WriteLine($"# of sentences: {countOfSentences}");
+            Console.WriteLine($"# of paragraphs: {countOfParagraphs}");
         }
     }
 }
