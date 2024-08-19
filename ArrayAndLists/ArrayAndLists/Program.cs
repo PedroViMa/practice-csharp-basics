7d9338a6c21b407b30b9d004ed045b13284f7010
Pedro Villa <pvillarrealmartinez@deloitte.com>
2024-08-19 09:31:22 -0600

    Adding solution for Arrays and Lists excercise.

--- ArrayAndLists/ArrayAndLists/Program.cs
+++ ArrayAndLists/ArrayAndLists/Program.cs
@@ -1,15 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ArrayAndLists
 {
     internal class Program
     {
+
+        static List<double> grades = new List<double>();
+
         static void Main(string[] args)
         {
+            Console.WriteLine("=================================");
+            Console.WriteLine("======== Students Grades ========");
+            Console.WriteLine("=================================");
+            Console.Write("Please enter the number of students in your class: ");
+            int numberOfStudents = Convert.ToInt32(Console.ReadLine());
+
+            AddGrades(numberOfStudents);
+            ShowGrades();
+        }
+
+        static void AddGrades(int numberOfStudents)
+        {
+            for (int i = 1; i <= numberOfStudents; i++)
+            {
+                Console.Write("Enter the grade for the student {0}: ", i);
+                grades.Add(Convert.ToDouble(Console.ReadLine()));
+            }
+        }
+
+        static void ShowGrades()
+        {
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Class average: {0}", grades.Average());
+            Console.WriteLine("Class max: {0}", grades.Max());
+            Console.WriteLine("Class min: {0}", grades.Min());
+            Console.WriteLine("---------------------");
+
+            grades.Sort();
+            foreach (double grade in grades)
+            {
+                Console.WriteLine($"{grade}");
+            }
         }
     }
 }
