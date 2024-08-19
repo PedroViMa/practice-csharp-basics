c26ad437fb98691a8a795cb9fe094f2ef22b9821
Pedro Villa <pvillarrealmartinez@deloitte.com>
2024-08-19 09:48:02 -0600

    Adding solution for Dates exercise.

--- Dates/Dates/Program.cs
+++ Dates/Dates/Program.cs
@@ -10,6 +10,21 @@ namespace Dates
     {
         static void Main(string[] args)
         {
+            string date;
+            Console.WriteLine("=======================");
+            Console.WriteLine("======== Dates ========");
+            Console.WriteLine("=======================");
+            Console.Write("Please enter the first date (MM/DD/YYYY): ");
+            date = Console.ReadLine();
+            DateTime startDate = Convert.ToDateTime(date);
+            Console.Write("Please enter the second date (MM/DD/YYYY): ");
+            date = Console.ReadLine();
+            DateTime endDate = Convert.ToDateTime(date);
+
+            int days = (startDate - endDate).Days;
+
+            Console.WriteLine();
+            Console.WriteLine($"There are {Math.Abs(days)} days between {startDate.ToShortDateString()} and {endDate.ToShortDateString()}.");
         }
     }
 }
